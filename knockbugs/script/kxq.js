54e8a6ae3251a51dce046cd7f22ecc18899dc351
manxisuo <manxisuo@gmail.com>
2014-08-24 18:24:59 +0800

    Revert "试背景图片变化"
    This reverts commit 6e7c4ab30c03d4fc57bcb80bac90e70ce37204b1.

--- knockbugs/script/kxq.js
+++ knockbugs/script/kxq.js
@@ -7,12 +7,11 @@ var HEIGHT;
 
 // 全局数据
 var bg_img;
-var canvas_pattern;
-var canvas_gradient;
-var bg_list = [undefined, 'floor.png', 'floor2.jpg', 'floor3.jpeg', 'floor4.jpg'];
+var bg_pattern;
+var bg_gradient;
 var image;
 var brush;
-var gameCount = 0; // 关卡数
+var gameCount = 0;
 
 // 每次游戏(关卡)的数据
 var bugs = [];
@@ -58,10 +57,10 @@ function init() {
 
 	image = $('#image_source')[0];
 	bg_img = $('#bg_img')[0];
-	canvas_pattern = ctx.createPattern(bg_img, 'repeat');
-	canvas_gradient = ctx.createLinearGradient(0, 0, WIDTH, HEIGHT);
-	canvas_gradient.addColorStop(0, 'rgb(0, 137, 40)');
-	canvas_gradient.addColorStop(1, 'rgb(0, 137, 143)');
+	bg_pattern = ctx.createPattern(bg_img, 'repeat');
+	bg_gradient = ctx.createLinearGradient(0, 0, WIDTH, HEIGHT);
+	bg_gradient.addColorStop(0, 'rgb(0, 137, 40)');
+	bg_gradient.addColorStop(1, 'rgb(0, 137, 143)');
 
 	PopWin.init();
 
@@ -116,9 +115,6 @@ function newGame() {
 		bugs.push(makeBug());
 	}
 
-	// 设置背景
-	setBackground();
-
 	// 周期任务
 	timerId = setInterval(draw, INTERVAL);
 
@@ -130,19 +126,8 @@ function endGame() {
 	gameCount = 0;
 }
 
-function setBackground() {
-//	var index = Util.rndRange(0, bg_list.length);
-	var index = gameCount % bg_list.length;
-	var image = bg_list[index] ? 'url(image/' + bg_list[index] + ')' : '';
-	$('html').css('background-image', image);
-}
-
-function resetBackground() {
-	$('html').css('background-image', '');
-}
-
 function draw() {
-	clearCanvas();
+	drawBackground();
 
 	var bug;
 	for ( var i = 0; i < bugs.length; i++) {
@@ -171,9 +156,9 @@ function draw() {
 	}
 }
 
-function clearCanvas() {
-	// brush.clear(canvas_pattern);
-	// brush.clear(canvas_gradient);
+function drawBackground() {
+	// brush.clear(bg_pattern);
+	// brush.clear(bg_gradient);
 	brush.clear();
 }
 
@@ -190,8 +175,8 @@ function drawWelcomePage() {
 	var angle = 0;
 	var timerId = setInterval(function() {
 		angle += Math.PI / 12;
-		clearCanvas();
-		brush.strokeText('一起打小强', WIDTH / 2 - 125, 100, '50px sans-serif');
+		drawBackground();
+		brush.fillText('一起打小强', WIDTH / 2 - 125, 100, '50px sans-serif');
 		brush.image(image, WIDTH / 2, HEIGHT / 2, 200, 200, -Math.PI / 2
 				+ angle);
 	}, 50);
@@ -202,6 +187,5 @@ function drawWelcomePage() {
 		newGame();
 	};
 	canvas.addEventListener('touchstart', fn);
-	
-	resetBackground();
+
 }
